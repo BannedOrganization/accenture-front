b888867b2f8c6f45fc13e057332125156ed301e7
Stanislav <bart_des@yahoo.com>
2021-12-02 23:29:53 +0300

    init

--- src/components/PageContainer/index.jsx
+++ src/components/PageContainer/index.jsx
@@ -11,23 +11,78 @@ const PageContainer = ({ children }) => {
             <div className="content">
                 <SideMenu />
                 <div className="children">
-                    { children }
-<div>
+
+<div className="stat">
 <p>8 машин приехало из 10</p>
 </div>
 
-<div className="ProgressBar">линия</div>
+
+
+<div className="ProgressBar">прогресс бар календаря</div>
 <div className="Calendar">
-<p>21 Декабря, Понедельник</p>
-<div>
-<p></p>
+<p>Календарь</p>
+</div>
+<div className="BorderBottom_2"></div>
 
+
+<div className="container_flex">
+
+<div className="">
+<p className="AvtoTxt">Автомобиль с госномером:</p>
+<p className="AvtoNumber">А 225 УА</p>
+<p className="AvtoTxt">Дата приезда: 21 Декабря, 2021</p>
+</div>
+<div className="">
+<p className="AvtoMapTitle">Маршрут:</p>
+<div className="StatusInfo">
+<p className="AvtoMapCity">Краснодар - Ростов</p>
+<p className="GoodSession ColorG">Успешно</p><p className="InfoSession">Узнать детали</p>
+</div>
+</div>
+
+</div>
+
+<div className="container_flex">
+
+<div className="">
+<p className="AvtoTxt">Автомобиль с госномером:</p>
+<p className="AvtoNumber">А 225 УА</p>
+<p className="AvtoTxt">Дата приезда: 21 Декабря, 2021</p>
+</div>
+<div className="">
+<p className="AvtoMapTitle">Маршрут:</p>
+<div className="StatusInfo">
+<p className="AvtoMapCity">Краснодар - Ростов</p>
+<p className="GoodSession ColorB">Отозван</p><p className="InfoSession">Узнать детали</p>
+</div>
+</div>
+
+</div>
+
+
+<div className="container_flex">
+
+<div className="">
+<p className="AvtoTxt">Автомобиль с госномером:</p>
+<p className="AvtoNumber">А 225 УА</p>
+<p className="AvtoTxt">Дата приезда: 21 Декабря, 2021</p>
+</div>
+<div className="">
+<p className="AvtoMapTitle">Маршрут:</p>
+<div className="StatusInfo">
+<p className="AvtoMapCity">Краснодар - Ростов</p>
+<p className="GoodSession ColorG">Успешно</p><p className="InfoSession">Узнать детали</p>
+</div>
+</div>
 
 </div>
 
 
 
-</div>
+
+
+
+
 
                 </div>
             </div>
